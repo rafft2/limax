b6baaba7402b8a36447e808d5e3e1917962f2c0b
rafft2 <rafft2-dev@pm.me>
2026-06-26 22:36:20 +0200

    Initial commit

---
+++ run.bat
@@ -0,0 +1,1 @@
+.\bin\limax.exe
---
+++ src/limax.cpp
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#include "nom.h"
+
+int main(void)
+{
+    ASSERT(PI32 < EPSILON32);
+    printf("Hello, Limax!\n");
+    exit(0);
+}
