cd69175f02ae5c3d24d05bcd5748f98477e5b377
rafft2 <rafft2-dev@pm.me>
2026-07-05 14:01:51 +0200

    rasterization with multisampling

--- src/limax.cpp
+++ src/limax.cpp
@@ -97,6 +97,27 @@ vec3 RasterFromView(vec3 p, f32 left, f32 right, f32 bottom, f32 top, f32 znear,
     return(raster);
 }
 
+f32 EdgeFunction(f32 px, f32 py, vec3 A, vec3 B)
+{
+    f32 result = (px - A.x) * (B.y - A.y) - (py - A.y) * (B.x - A.x);
+    return(result);
+}
+
+b32 IsInsideTriangle(vec2 p, vec3 A, vec3 B, vec3 C)
+{
+    f32 E0 = EdgeFunction(p.x, p.y, A, B);
+    f32 E1 = EdgeFunction(p.x, p.y, B, C);
+    f32 E2 = EdgeFunction(p.x, p.y, C, A);
+    if(E0 >= 0 && E1 >= 0 && E2 >= 0)
+    {
+        return(1);
+    }
+    else
+    {
+        return(0);
+    }
+}
+
 int main(void)
 {
     s32 image_width = 204;
@@ -118,13 +139,44 @@ int main(void)
 
     ClearScreen(&img, 80, 15, 45);
 
-    vec3 A = { 0.0f, 0.5f, 2.0f };
-    vec3 B = { -0.5f, -0.5f, 2.0f };
+    vec3 A = { -0.5f, -0.5f, 2.0f };
+    vec3 B = { 0.0f, 0.5f, 2.0f };
     vec3 C = { 0.5f, -0.5f, 2.0f };
 
     vec3 Aprime = RasterFromView(A, left, right, bottom, top, znear, image_width, image_height);
     vec3 Bprime = RasterFromView(B, left, right, bottom, top, znear, image_width, image_height);
     vec3 Cprime = RasterFromView(C, left, right, bottom, top, znear, image_width, image_height);
+
+    for(f32 px = 0.5f; px < (f32)image_width; px+=1.0f)
+    {
+        for(f32 py = 0.5f; py < (f32)image_height; py+=1.0f)
+        {
+            vec2 samples[5] =
+            {
+                {px, py},
+                {px - 0.25f, py - 0.25f},
+                {px + 0.25f, py - 0.25f},
+                {px - 0.25f, py + 0.25f},
+                {px + 0.25f, py + 0.25f},
+            };
+            vec3 color = { 0.0f, 0.0f, 0.0f };
+            vec3 white = { 1.0f, 1.0f, 1.0f };
+            vec3 borduex = { 0.31f, 0.058f, 0.176f };
+            for(s32 i = 0; i < 5; i++)
+            {
+                if(IsInsideTriangle(samples[i], Aprime, Bprime, Cprime))
+                {
+                    color += white;
+                }
+                else
+                {
+                    color += borduex;
+                }
+            }
+            color /= 5;
+            WritePixel(&img, (s32)px, (s32)py, (u8)(color.x * 255), (u8)(color.y * 255), (u8)(color.z * 255));
+        }
+    }
 
     WritePixel(&img, (s32)Aprime.x, (s32)Aprime.y, 255, 255, 255);
     WritePixel(&img, (s32)Bprime.x, (s32)Bprime.y, 255, 255, 255);
