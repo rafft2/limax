a6d8bd80dbf9210c050ec4b2b8e9e476727592dd
rafft2 <rafft2-dev@pm.me>
2026-07-05 21:46:48 +0200

    weird cow, revisited

--- src/limax.cpp
+++ src/limax.cpp
@@ -40,6 +40,25 @@ image CreateImage(s32 width, s32 height)
     return(result);
 }
 
+f32 GetDepth(image *img, s32 x, s32 y)
+{
+    if(x < 0 || y < 0 || x >= img->width || y >= img->height)
+    {
+        return FLT_MAX;
+    }
+    f32 depth = img->zbuffer[y * img->width + x];
+    return(depth);
+}
+
+void WriteDepth(image *img, s32 x, s32 y, f32 depth)
+{
+    if(x < 0 || y < 0 || x >= img->width || y >= img->height)
+    {
+        return;
+    }
+    img->zbuffer[y * img->width + x] = depth;
+}
+
 void WritePixel(image *img, s32 x, s32 y, u8 r, u8 g, u8 b)
 {
     if(x < 0 || y < 0 || x >= img->width || y >= img->height)
@@ -79,7 +98,7 @@ void WriteImageToFile(image *img, const char *filename)
 
 vec3 RasterFromView(vec3 p, f32 left, f32 right, f32 bottom, f32 top, f32 znear, s32 image_width, s32 image_height)
 {
-    vec3 raster;
+    vec3 raster = { FLT_MAX, FLT_MAX, FLT_MAX };
 
     // screen from view
     raster.x = (p.x * znear) / -p.z;
@@ -92,36 +111,188 @@ vec3 RasterFromView(vec3 p, f32 left, f32 right, f32 bottom, f32 top, f32 znear,
 
     // raster from NDC
     raster.x = ((raster.x + 1.0f) / 2.0f) * ((f32)image_width - 1.0f);
-    raster.y = ((raster.y + 1.0f) / 2.0f) * ((f32)image_height - 1.0f);;
+    raster.y = ((-raster.y + 1.0f) / 2.0f) * ((f32)image_height - 1.0f);
 
     return(raster);
 }
 
-f32 EdgeFunction(f32 px, f32 py, vec3 A, vec3 B)
+f32 GetTriangleArea(vec3 P, vec3 A, vec3 B)
 {
-    f32 result = (px - A.x) * (B.y - A.y) - (py - A.y) * (B.x - A.x);
+    f32 result = (A.x - B.x) * (P.y - A.y) - (A.y - B.y) * (P.x - A.x);
     return(result);
 }
 
-b32 IsInsideTriangle(vec2 p, vec3 A, vec3 B, vec3 C)
+struct rect2
 {
-    f32 E0 = EdgeFunction(p.x, p.y, A, B);
-    f32 E1 = EdgeFunction(p.x, p.y, B, C);
-    f32 E2 = EdgeFunction(p.x, p.y, C, A);
-    if(E0 >= 0 && E1 >= 0 && E2 >= 0)
+    vec2 min_corner;
+    vec2 max_corner;
+};
+
+rect2 GetTriangleBounds(vec3 A, vec3 B, vec3 C)
+{
+    f32 min_x = MIN(A.x, MIN(B.x, C.x));
+    f32 max_x = MAX(A.x, MAX(B.x, C.x));
+    f32 min_y = MIN(A.y, MIN(B.y, C.y));
+    f32 max_y = MAX(A.y, MAX(B.y, C.y));
+    rect2 result;
+    result.min_corner = { min_x, min_y };
+    result.max_corner = { max_x, max_y };
+    return(result);
+}
+
+vec3 background_color = { 0.31f, 0.058f, 0.176f };
+void DrawTriangle(image *img, vec3 V0, vec3 V1, vec3 V2)
+{
+    f32 total_area = GetTriangleArea(V0, V1, V2);
+    if(NearZero(total_area))
     {
-        return(1);
+        return;
     }
-    else
+    rect2 bbox = GetTriangleBounds(V0, V1, V2);
+    s32 min_x = MAX((s32)(bbox.min_corner.x), 0);
+    s32 min_y = MAX((s32)(bbox.min_corner.y), 0);
+    s32 max_x = MIN((s32)ceilf(bbox.max_corner.x), img->width - 1);
+    s32 max_y = MIN((s32)ceilf(bbox.max_corner.y), img->height - 1);
+
+    for(s32 x = min_x; x <= max_x; x++)
     {
-        return(0);
+        for(s32 y = min_y; y <= max_y; y++)
+        {
+            vec3 rasterP = { (f32)x + 0.5f, (f32)y + 0.5f, FLT_MAX };
+            vec3 samples[4]
+            {
+                { rasterP.x - 0.25f, rasterP.y - 0.25f },
+                { rasterP.x - 0.25f, rasterP.y + 0.25f },
+                { rasterP.x + 0.25f, rasterP.y - 0.25f },
+                { rasterP.x + 0.25f, rasterP.y + 0.25f },
+            };
+            vec3 color = { 0.0f, 0.0f, 0.0f };
+            f32 closest_depth = FLT_MAX;
+            for(s32 i = 0; i < 4; i++)
+            {
+                vec3 P = samples[i];
+                f32 w0 = GetTriangleArea(P, V1, V2);
+                f32 w1 = GetTriangleArea(P, V2, V0);
+                f32 w2 = GetTriangleArea(P, V0, V1);
+                if(w0 >= 0 && w1 >= 0 && w2 >= 0)
+                {
+                    w0 /= total_area;
+                    w1 /= total_area;
+                    w2 /= total_area;
+                    vec3 tmp = { w0, w1, w2 };
+                    color += tmp;
+                    f32 depth = V0.z + w1 * (V1.z - V0.z) + w2 * (V2.z - V0.z);
+                    if(depth < closest_depth)
+                    {
+                        closest_depth = depth;
+                    }
+                }
+                else
+                {
+                    color += background_color;
+                }
+            }
+            color /= 4;
+            if(closest_depth < GetDepth(img, x, y))
+            {
+                WritePixel(img, x, y, (u8)(color.x * 255), (u8)(color.y * 255), (u8)(color.z * 255));
+                WriteDepth(img, x, y, closest_depth);
+            }
+        }
+    }
+}
+
+struct triangle_mesh
+{
+    vec3 *vertices;
+    u32 vertex_count;
+    u32 *indices;
+    u32 index_count; 
+};
+
+triangle_mesh CreateMeshFromFile(const char *filename)
+{
+    triangle_mesh mesh = {};
+    FILE *f = fopen(filename, "r");
+    if(!f)
+    {
+        printf("cannot open file: %s.\n", filename);
+        return(mesh);
+    }
+    char line[256];
+    u32 vertex_count = 0;
+    u32 index_count = 0;
+    while(fgets(line, 128, f) != NULL)
+    {
+        if(line[0] == 'v')
+        {
+            vertex_count++;
+        }
+        else if(line[0] == 'f')
+        {
+            index_count+=3;
+        }
+    }
+    fseek(f, 0, SEEK_SET);
+    vec3 *vertices = (vec3*)malloc((vertex_count + 1) * sizeof(vec3));
+    u32 *indices = (u32*)malloc(index_count * sizeof(u32));
+    u32 vi = 1;
+    u32 fi = 0;
+
+    while(fgets(line, 256, f) != NULL)
+    {
+        if(line[0] == 'v')
+        {
+            f32 x, y, z;
+            if(sscanf(line + 1, "%f %f %f", &x, &y, &z) != 3)
+            {
+                printf("error.\n");
+            }
+            vertices[vi++] = {x, y, z};
+        }
+        else if(line[0] == 'f')
+        {
+            u32 a, b, c;
+            if(sscanf(line + 1, "%u %u %u", &a, &b, &c) != 3)
+            {
+                // TODO: handle format with /
+                printf("error.\n");
+            }
+            indices[fi++] = a;
+            indices[fi++] = b;
+            indices[fi++] = c;
+        }
+    }
+    fclose(f);
+
+    mesh.vertex_count = vertex_count;
+    mesh.index_count = index_count;
+    mesh.vertices = vertices;
+    mesh.indices = indices;
+    return(mesh);
+}
+
+void DrawMesh(image *img, triangle_mesh *mesh, f32 left, f32 right, f32 bottom, f32 top, f32 znear)
+{
+    mat4x4 transform = Scaling(0.2f) * RotationY(-45.0f) * Translation(0.0f, 0.0f, -1.5f);
+    for(u32 i = 0; i < mesh->index_count; i+=3)
+    {
+        u32 idx0 = mesh->indices[i]; 
+        u32 idx1 = mesh->indices[i + 1];
+        u32 idx2 = mesh->indices[i + 2];
+
+        vec3 V0 = RasterFromView(transform * mesh->vertices[idx0], left, right, bottom, top, znear, img->width, img->height);
+        vec3 V1 = RasterFromView(transform * mesh->vertices[idx1], left, right, bottom, top, znear, img->width, img->height);
+        vec3 V2 = RasterFromView(transform * mesh->vertices[idx2], left, right, bottom, top, znear, img->width, img->height);
+
+        DrawTriangle(img, V0, V1, V2);
     }
 }
 
 int main(void)
 {
-    s32 image_width = 204;
-    s32 image_height = 155;
+    s32 image_width = 1920;
+    s32 image_height = 1080;
 
     f32 aspect_ratio = (f32)image_width / (f32)image_height;
     f32 znear = 0.1f;
@@ -137,50 +308,10 @@ int main(void)
 
     image img = CreateImage(image_width, image_height);
 
-    ClearScreen(&img, 80, 15, 45);
+    ClearScreen(&img, (u8)(background_color.x * 255), (u8)(background_color.y * 255), (u8)(background_color.z * 255));
 
-    vec3 A = { -0.5f, -0.5f, 2.0f };
-    vec3 B = { 0.0f, 0.5f, 2.0f };
-    vec3 C = { 0.5f, -0.5f, 2.0f };
-
-    vec3 Aprime = RasterFromView(A, left, right, bottom, top, znear, image_width, image_height);
-    vec3 Bprime = RasterFromView(B, left, right, bottom, top, znear, image_width, image_height);
-    vec3 Cprime = RasterFromView(C, left, right, bottom, top, znear, image_width, image_height);
-
-    for(f32 px = 0.5f; px < (f32)image_width; px+=1.0f)
-    {
-        for(f32 py = 0.5f; py < (f32)image_height; py+=1.0f)
-        {
-            vec2 samples[5] =
-            {
-                {px, py},
-                {px - 0.25f, py - 0.25f},
-                {px + 0.25f, py - 0.25f},
-                {px - 0.25f, py + 0.25f},
-                {px + 0.25f, py + 0.25f},
-            };
-            vec3 color = { 0.0f, 0.0f, 0.0f };
-            vec3 white = { 1.0f, 1.0f, 1.0f };
-            vec3 borduex = { 0.31f, 0.058f, 0.176f };
-            for(s32 i = 0; i < 5; i++)
-            {
-                if(IsInsideTriangle(samples[i], Aprime, Bprime, Cprime))
-                {
-                    color += white;
-                }
-                else
-                {
-                    color += borduex;
-                }
-            }
-            color /= 5;
-            WritePixel(&img, (s32)px, (s32)py, (u8)(color.x * 255), (u8)(color.y * 255), (u8)(color.z * 255));
-        }
-    }
-
-    WritePixel(&img, (s32)Aprime.x, (s32)Aprime.y, 255, 255, 255);
-    WritePixel(&img, (s32)Bprime.x, (s32)Bprime.y, 255, 255, 255);
-    WritePixel(&img, (s32)Cprime.x, (s32)Cprime.y, 255, 255, 255);
+    triangle_mesh cow = CreateMeshFromFile("assets/cow.obj");
+    DrawMesh(&img, &cow, left, right, bottom, top, znear);
 
     WriteImageToFile(&img, "out.png");
 
